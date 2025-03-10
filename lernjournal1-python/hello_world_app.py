c9571c85453d76123bec95ee2ed1ae1941907f7a
gsparty <daniel.brody00@gmail.com>
2025-03-10 13:54:44 +0100

    1. Azure deployment, hello_world_app

---
+++ lernjournal1-python/hello_world_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    response = None
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        # Eine einfache Antwortfunktion, die den Text umkehrt
+        response = user_input[::-1]  # Hier kannst du auch eine komplexere Antwortlogik einbauen
+    return render_template('index.html', response=response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
