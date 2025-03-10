0b2d85860529935b67c5d6e89e46a0e9b197c9cb
gsparty <daniel.brody00@gmail.com>
2025-03-10 16:38:16 +0100

    Finally deployed (Hosted in canada? Cant change)

--- lernjournal1-python/hello_world_app.py
+++ lernjournal1-python/hello_world_app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -7,13 +9,13 @@ def index():
     response = None
     if request.method == 'POST':
         user_input = request.form['user_input']
-        # Eine einfache Antwortfunktion, die den Text umkehrt
-        response = user_input[::-1]  # Hier kannst du auch eine komplexere Antwortlogik einbauen
+        response = user_input[::-1]  
     return render_template('index.html', response=response)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
